e53350d8f40355d0d8076d622ab0f67b0bb08900
manishsharma23 <35147064+manishsharma23@users.noreply.github.com>
2019-08-20 20:18:45 +0530

    Update ESDocumentController.java

--- SPRINGBOOT2_ELASTIC6_RESTCLIENT/src/main/java/com/example/elasticsearch/ESDocumentController.java
+++ SPRINGBOOT2_ELASTIC6_RESTCLIENT/src/main/java/com/example/elasticsearch/ESDocumentController.java
@@ -2,7 +2,7 @@ package com.example.elasticsearch;
 
 import java.net.InetAddress;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashMap; 
 import java.util.Map;
 
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
